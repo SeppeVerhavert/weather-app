42478e5d259fe4d9aa71a9f5a16d7f9b96cb3bd5
seppe_verhavert <seppe.verhavert@gmail.com>
2019-07-01 14:19:30 +0200

    add icon and short weather description

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
-let searchTerm = document.getElementById('searchInput').value;
-
-let appId='51edef6c4976ee55fb94fa75f9d29de3';
-let units='metric';
-let searchMethod='zip';
+let appId = '51edef6c4976ee55fb94fa75f9d29de3';
+let units = 'imperial';
+let searchMethod;
 
 document.getElementById('searchBtn').addEventListener('click', checkInput);
 
-function searchWeather(searchTerm) {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`).then(result => {
-        return result.json();
-    }).then(result => {
-        init(result);
-    }); 
+function getSearchOnMethod(searchTerm) {
+    if (searchTerm.length <= 5 && Number.parseInt(searchTerm) + '' === searchTerm) {
+        searchMethod = "zip";
+    } else {
+        searchMethod = "q";
+    }
 }
 
+function searchWeather(searchTerm) {
+    getSearchOnMethod(searchTerm);
+    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`)
+        .then((result) => {
+            return result.json();
+        }).then((res) => {
+            init(res);
+        });
+}
 function init(resultFromServer) {
-    console.log(resultFromServer);
+    let weatherDescriptionHeader = document.getElementById("weatherDiscriptionHeader");
+    let temperatureElement = document.getElementById("temperatureElement");
+    let humidityElement = document.getElementById("humidityElement");
+    let windspeedElement = document.getElementById("windspeedElement");
+    let cityHeader = document.getElementById("cityHeader");
+    let weatherIcon = document.getElementById("documentIconImg");
+
+    weatherIcon.src = 'http://openweathermap.org/img/w/' + resultFromServer.weather[0].icon + '.png';
+
+    let resultDesciption = resultFromServer.weather[0].description;
+    weatherDescriptionHeader.innerText = resultDesciption;
 }
 
 function checkInput() {
-    console.log("yay");
-    if (searchTerm) {
-        searchWeather();
-    }
+    let searchTerm = document.getElementById('searchInput').value;
+    if (searchTerm)
+        searchWeather(searchTerm);
 }
