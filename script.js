d6dbda05038351482464dc75cd7eb33e72a54d0b
seppe_verhavert <seppe.verhavert@gmail.com>
2019-07-02 10:20:11 +0200

    only show correct icon on click

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let appId = '51edef6c4976ee55fb94fa75f9d29de3';
 let units = 'imperial';
-let searchMethod;
+let searchMethod = "q";
 
 let temperatureElement = document.getElementById("temperature");
 let cityHeader = document.getElementById("cityHeader");
@@ -8,19 +8,11 @@ let weatherDescriptionHeader = document.getElementById("weatherDiscriptionHeader
 let weatherIcon = document.getElementById("documentIconImg");
 
 let Icons = ['Clear', 'Clouds', 'Rain', 'Drizzle', 'Thunderstorm', 'Fog', 'Snow'];
+let iconDiv = document.getElementById("iconDiv");
 
 document.getElementById('searchBtn').addEventListener('click', checkInput);
 
-function getSearchOnMethod(searchTerm) {
-    if (searchTerm.length <= 5 && Number.parseInt(searchTerm) + '' === searchTerm) {
-        searchMethod = "zip";
-    } else {
-        searchMethod = "q";
-    }
-}
-
 function searchWeather(searchTerm) {
-    getSearchOnMethod(searchTerm);
     fetch(`http://api.openweathermap.org/data/2.5/forecast?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`)
         .then((result) => {
             return result.json();
@@ -55,29 +47,29 @@ function checkInput() {
 }
 
 function applyIcon() {
-    if 
-    (weatherIcon === "01d") { //CLEAR SKY
-        document.querySelector(".fa-sun").style.display = "block";
-    } else if 
-    (weatherIcon === "02d") { //FEW CLOUDS
-        document.querySelector(".fa-cloud-sun").style.display = "block";
-    } else if 
-    (weatherIcon === "03d" || weatherIcon === "04d") { //SCATTERED OR BROKEN CLOUDS
-        document.querySelector(".fa-cloud").style.display = "block";
-    } else if 
-    (weatherIcon === "09d") { //SHOWER RAIN
-        document.querySelector(".fa-cloud-showers-heavy").style.display = "block";
-    } else if 
-    (weatherIcon === "10d") { //RAIN
-        document.querySelector(".fa-cloud-sun-rain").style.display = "block";
-    } else if 
-    (weatherIcon === "11d") { //THUNDERSTORM
-        document.querySelector(".fa-bolt").style.display = "block";
-    } else if 
-    (weatherIcon === "13d") { //SNOW
-        document.querySelector(".fa-snowflake").style.display = "block";
-    } else if 
-    (weatherIcon === "50d") { //FOG
-        document.querySelector(".fa-smog").style.display = "block";
+    if
+        (weatherIcon === "01d") { //CLEAR SKY
+        iconDiv.innerHTML = '<i class="fas fa-sun"></i>';
+    } else if
+        (weatherIcon === "02d") { //FEW CLOUDS
+        iconDiv.innerHTML = '<i class="fas fa-cloud-sun"></i>';
+    } else if
+        (weatherIcon === "03d" || weatherIcon === "04d") { //SCATTERED OR BROKEN CLOUDS
+        iconDiv.innerHTML = '<i class="fas fa-cloud"></i>';
+    } else if
+        (weatherIcon === "09d") { //SHOWER RAIN
+        iconDiv.innerHTML = '<i class="fas fa-cloud-sun-rain"></i>';
+    } else if
+        (weatherIcon === "10d") { //RAIN
+        iconDiv.innerHTML = '<i class="fas fa-cloud-showers-heavy"></i>';
+    } else if
+        (weatherIcon === "11d") { //THUNDERSTORM
+        iconDiv.innerHTML = '<i class="fas fa-bolt"></i>';
+    } else if
+        (weatherIcon === "13d") { //SNOW
+        iconDiv.innerHTML = '<i class="far fa-snowflake"></i>';
+    } else if
+        (weatherIcon === "50d") { //FOG
+        iconDiv.innerHTML = '<i class="fas fa-smog"></i>';
     }
 }
